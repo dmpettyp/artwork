6db290544142f12312e0d17c8b9b911b0efe4c9c
Dave Pettypiece <dave@pettypiece.com>
2025-09-29 20:17:26 -0400

    more unit tests

--- domain/imagegraph/imagegraph_test.go
+++ domain/imagegraph/imagegraph_test.go
@@ -96,3 +96,113 @@ func TestNewImageGraph(t *testing.T) {
 		}
 	})
 }
+
+func TestImageGraph_AddNode(t *testing.T) {
+	t.Run("adds node with valid parameters", func(t *testing.T) {
+		ig, _ := imagegraph.NewImageGraph(imagegraph.MustNewImageGraphID(), "test")
+		nodeID := imagegraph.MustNewNodeID()
+
+		err := ig.AddNode(nodeID, imagegraph.NodeTypeInput, "input-node")
+
+		if err != nil {
+			t.Fatalf("expected no error, got %v", err)
+		}
+
+		if len(ig.Nodes) != 1 {
+			t.Errorf("expected 1 node, got %d", len(ig.Nodes))
+		}
+
+		node, exists := ig.Nodes.Get(nodeID)
+		if !exists {
+			t.Fatal("expected node to exist")
+		}
+
+		if node.ID != nodeID {
+			t.Errorf("expected node ID %v, got %v", nodeID, node.ID)
+		}
+
+		if node.Name != "input-node" {
+			t.Errorf("expected node name %q, got %q", "input-node", node.Name)
+		}
+
+		if node.Type != imagegraph.NodeTypeInput {
+			t.Errorf("expected node type %v, got %v", imagegraph.NodeTypeInput, node.Type)
+		}
+	})
+
+	t.Run("increments version when node added", func(t *testing.T) {
+		ig, _ := imagegraph.NewImageGraph(imagegraph.MustNewImageGraphID(), "test")
+		initialVersion := ig.Version
+
+		err := ig.AddNode(imagegraph.MustNewNodeID(), imagegraph.NodeTypeInput, "node")
+
+		if err != nil {
+			t.Fatalf("expected no error, got %v", err)
+		}
+
+		if ig.Version != initialVersion+2 {
+			t.Errorf("expected version %v, got %v", initialVersion+1, ig.Version)
+		}
+	})
+
+	t.Run("emits NodeAdded event", func(t *testing.T) {
+		ig, _ := imagegraph.NewImageGraph(imagegraph.MustNewImageGraphID(), "test")
+		ig.ResetEvents()
+		nodeID := imagegraph.MustNewNodeID()
+
+		err := ig.AddNode(nodeID, imagegraph.NodeTypeInput, "node")
+
+		if err != nil {
+			t.Fatalf("expected no error, got %v", err)
+		}
+
+		events := ig.GetEvents()
+		// Should emit NodeCreatedEvent and NodeAddedEvent
+		if len(events) < 2 {
+			t.Fatalf("expected at least 2 events, got %d", len(events))
+		}
+	})
+
+	t.Run("returns error for duplicate node ID", func(t *testing.T) {
+		ig, _ := imagegraph.NewImageGraph(imagegraph.MustNewImageGraphID(), "test")
+		nodeID := imagegraph.MustNewNodeID()
+
+		err := ig.AddNode(nodeID, imagegraph.NodeTypeInput, "node1")
+		if err != nil {
+			t.Fatalf("expected no error on first add, got %v", err)
+		}
+
+		err = ig.AddNode(nodeID, imagegraph.NodeTypeInput, "node2")
+		if err == nil {
+			t.Fatal("expected error for duplicate node ID, got nil")
+		}
+	})
+
+	t.Run("returns error for invalid node type", func(t *testing.T) {
+		ig, _ := imagegraph.NewImageGraph(imagegraph.MustNewImageGraphID(), "test")
+
+		err := ig.AddNode(imagegraph.MustNewNodeID(), imagegraph.NodeTypeNone, "node")
+
+		if err == nil {
+			t.Fatal("expected error for invalid node type, got nil")
+		}
+	})
+
+	t.Run("can add multiple nodes", func(t *testing.T) {
+		ig, _ := imagegraph.NewImageGraph(imagegraph.MustNewImageGraphID(), "test")
+
+		err := ig.AddNode(imagegraph.MustNewNodeID(), imagegraph.NodeTypeInput, "node1")
+		if err != nil {
+			t.Fatalf("expected no error adding node1, got %v", err)
+		}
+
+		err = ig.AddNode(imagegraph.MustNewNodeID(), imagegraph.NodeTypeInput, "node2")
+		if err != nil {
+			t.Fatalf("expected no error adding node2, got %v", err)
+		}
+
+		if len(ig.Nodes) != 2 {
+			t.Errorf("expected 2 nodes, got %d", len(ig.Nodes))
+		}
+	})
+}
