8551166ba1f7d17d5e17b9f53f3bb2ad1cd9a05e
Dave Pettypiece <dave@pettypiece.com>
2025-10-07 19:44:34 -0400

    added shell script to drive create endpoint

---
+++ scripts/create-imagegraph.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Script to create a new ImageGraph via the HTTP API
+
+# Get name from command line argument, or use default
+NAME="${1:-Test ImageGraph}"
+
+echo "Creating ImageGraph with name: $NAME"
+echo ""
+
+curl -v -X POST http://localhost:8080/imagegraphs \
+  -H "Content-Type: application/json" \
+  -d "{\"name\":\"$NAME\"}"
+
+echo ""
